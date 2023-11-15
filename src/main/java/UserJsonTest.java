5ab7926256e02ccc940df7162b546f48d8d64cc9
Jairo Nascimento <jnasa01@hotmail.com>
2023-11-15 14:05:49 -0300

    Update_01_15112023

--- src/main/java/UserJsonTest.java
+++ src/main/java/UserJsonTest.java
@@ -102,5 +102,23 @@ public class UserJsonTest {
 		 	.body("salary", contains(1234.5678f, 2500 , null))
 		 	;
 	}
+	
+	@Test
+	public void devoFazerVerificacoesAvancadas() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/users")
+		.then()
+		 	.statusCode(200)
+		 	.body("", hasSize(3))
+		 	.body("age.findAll{it <= 25}.size()", is(2))
+		 	.body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+		 	.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+		 	.body("findAll{it.age <= 25}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+		 	.body("findAll{it.age <= 25}[1].name", is("Ana Júlia"))
+		 	.body("findAll{it.age <= 25}[-2].name", is("Ana Júlia"))
+		 	;
+	;
+	}
 
 }
